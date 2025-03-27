560d429a347c7e93e283458e5cb5377256827370
humpbackduck <alanwyl111@gmail.com>
2025-03-27 12:46:58 +0800

    updated

---
+++ home.php
@@ -0,0 +1,140 @@
+<?php
+// We need to use sessions, so you should always initialize sessions using the below function
+session_start();
+// If the user is not logged in, redirect to the login page
+if (!isset($_SESSION['account_loggedin'])) {
+    header('Location: index.php');
+    exit;
+}
+
+include_once('esp-database.php');
+
+$result = getAllOutputs();
+$html_buttons = null;
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        if ($row["state"] == "1"){
+            $button_checked = "checked";
+        }
+        else {
+            $button_checked = "";
+        }
+        $html_buttons .= '<h3>' . $row["name"] . ' - Board '. $row["board"] . ' - GPIO ' . $row["gpio"] . ' (<i><a onclick="deleteOutput(this)" href="javascript:void(0);" id="' . $row["id"] . '">Delete</a></i>)</h3><label class="switch"><input type="checkbox" onchange="updateOutput(this)" id="' . $row["id"] . '" ' . $button_checked . '><span class="slider"></span></label>';
+    }
+}
+
+$result2 = getAllBoards();
+$html_boards = null;
+if ($result2) {
+    $html_boards .= '<h3>Boards</h3>';
+    while ($row = $result2->fetch_assoc()) {
+        $row_reading_time = $row["last_request"];
+        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+
+        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 7 hours"));
+        $html_boards .= '<p><strong>Board ' . $row["board"] . '</strong> - Last Request Time: '. $row_reading_time . '</p>';
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,minimum-scale=1">
+        <title>Home</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+        <header class="header">
+
+            <div class="wrapper">
+
+                <h1>Website Title</h1>
+                
+                <nav class="menu">
+                    <a href="home.php">Home</a>
+                    <a href="profile.php">Profile</a>
+                    <a href="logout.php">
+                        <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg>
+                        Logout
+                    </a>
+                </nav>
+
+            </div>
+
+        </header>
+
+        <div class="content">
+
+        <h2>ESP Output Control</h2>
+    <?php echo $html_buttons; ?>
+    <br><br>
+    <?php echo $html_boards; ?>
+    <br><br>
+    <div><form onsubmit="return createOutput();">
+        <h3>Create New Output</h3>
+        <label for="outputName">Name</label>
+        <input type="text" name="name" id="outputName"><br>
+        <label for="outputBoard">Board ID</label>
+        <input type="number" name="board" min="0" id="outputBoard">
+        <label for="outputGpio">GPIO Number</label>
+        <input type="number" name="gpio" min="0" id="outputGpio">
+        <label for="outputState">Initial GPIO State</label>
+        <select id="outputState" name="state">
+          <option value="0">0 = OFF</option>
+          <option value="1">1 = ON</option>
+        </select>
+        <input type="submit" value="Create Output">
+        <p><strong>Note:</strong> in some devices, you might need to refresh the page to see your newly created buttons or to remove deleted buttons.</p>
+    </form></div>
+
+    <script>
+        function updateOutput(element) {
+            var xhr = new XMLHttpRequest();
+            if(element.checked){
+                xhr.open("GET", "esp-outputs-action.php?action=output_update&id="+element.id+"&state=1", true);
+            }
+            else {
+                xhr.open("GET", "esp-outputs-action.php?action=output_update&id="+element.id+"&state=0", true);
+            }
+            xhr.send();
+        }
+
+        function deleteOutput(element) {
+            var result = confirm("Want to delete this output?");
+            if (result) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", "esp-outputs-action.php?action=output_delete&id="+element.id, true);
+                xhr.send();
+                alert("Output deleted");
+                setTimeout(function(){ window.location.reload(); });
+            }
+        }
+
+        function createOutput(element) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "esp-outputs-action.php", true);
+
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            xhr.onreadystatechange = function() {
+                if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                    alert("Output created");
+                    setTimeout(function(){ window.location.reload(); });
+                }
+            }
+            var outputName = document.getElementById("outputName").value;
+            var outputBoard = document.getElementById("outputBoard").value;
+            var outputGpio = document.getElementById("outputGpio").value;
+            var outputState = document.getElementById("outputState").value;
+            var httpRequestData = "action=output_create&name="+outputName+"&board="+outputBoard+"&gpio="+outputGpio+"&state="+outputState;
+            xhr.send(httpRequestData);
+        }
+    </script>
+        </div>
+
+    </body>
+</html>
